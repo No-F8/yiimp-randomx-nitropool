2a32fbd1157e621251098b2f6f2b69cbf32524e1
No-F8 <david.dorling@traceresources.com.au>
2025-11-16 15:29:37 +1000

    Fix GCC alignment in blake2s

--- sha3/blake2s.c
+++ sha3/blake2s.c
@@ -324,7 +324,8 @@ int blake2s_final( blake2s_state *S, uint8_t *out, uint8_t outlen )
 
 int blake2s( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen )
 {
-	blake2s_state S[1];
+        blake2s_state Sst;
+        blake2s_state *S = &Sst;
 
 	/* Verify parameters */
 	if ( NULL == in ) return -1;
